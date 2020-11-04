c114f95c5c072a0b78748f21c550ebeeecdd923d
niexiaobin <740126220@qq.com>
2020-11-04 10:59:49 +0800

    添加项目文件。

---
+++ Publisher/Program.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                //AutoOffsetReset = AutoOffsetReset.Earliest,//显式强类型赋值配置
+                BootstrapServers = "192.168.28.172:9093,192.168.28.172:9094,192.168.28.172:9095",
+                Acks = Acks.Leader
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+                Console.ReadKey();
+            };
+
+            //var builder = new ProducerBuilder<string, object>(conf);
+            //builder.SetValueSerializer(new KafkaConverter());//设置序列化方式
+            //var producer = builder.Build();
+            //producer.Produce("test", new Message<string, object>() { Key = "Test", Value = "hello world" });
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<string, object>(config).SetValueSerializer(new KafkaConverter()).Build())
+            {
+                try
+                {
+                    Console.WriteLine("请输入keys");
+                    while (!cts.IsCancellationRequested)
+                    {
+                        var keys = Console.ReadLine();
+                        var dr = p.ProduceAsync("mykafka", new Message<string, object> { Key = keys, Value = keys }, cancellationToken: cts.Token);
+                        dr.ContinueWith(task =>
+                        {
+                            Console.WriteLine($"Producer:{p.Name} ,Topic:{keys} ，Partition:{task.Result.Partition} ，Offset:{task.Result.Offset} ");
+                        });
+                        p.Flush(TimeSpan.FromSeconds(5));
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+
+    public class KafkaConverter : ISerializer<object>
+    {
+        /// <summary>
+        /// 序列化数据成字节
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public byte[] Serialize(object data, SerializationContext context)
+        {
+            var json = JsonConvert.SerializeObject(data);
+            return Encoding.UTF8.GetBytes(json);
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,71 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumerConfig config = new ConsumerConfig()
+            {
+                BootstrapServers = "192.168.28.172:9093,192.168.28.172:9094,192.168.28.172:9095",
+                GroupId = Guid.NewGuid().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                EnableAutoCommit = false
+            };
+
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, object>(config).SetValueDeserializer(new KafkaConverter()).Build())
+            {
+                //c.Assign(new TopicPartition("test", new Partition(1)));//从指定的Partition订阅消息使用Assign方法
+                consumer.Subscribe("mykafka");//订阅消息使用Subscribe方法
+                while (!cts.IsCancellationRequested)
+                {
+                    var result = consumer.Consume(cts.Token);
+                    if (result.Offset % 5 == 0)
+                    {
+                        consumer.Commit(result);//手动提交，如果上面的EnableAutoCommit=true表示自动提交，则无需调用Commit方法
+                        Console.WriteLine($"recieve message:{result.Message.Value},Committed offset: {result.Offset}");
+                    }
+                    else
+                        Console.WriteLine($"recieve message:{result.Message.Value}，Committed offset: { result.Offset }");
+
+                }
+            }
+        }
+    }
+
+    public class KafkaConverter : IDeserializer<object>
+    {/// <summary>
+     /// 反序列化字节数据成实体数据
+     /// </summary>
+     /// <param name="data"></param>
+     /// <param name="context"></param>
+     /// <returns></returns>
+        public object Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            if (isNull) return null;
+
+            var json = Encoding.UTF8.GetString(data.ToArray());
+            try
+            {
+                return JsonConvert.DeserializeObject(json);
+            }
+            catch
+            {
+                return json;
+            }
+        }
+    }
+}
